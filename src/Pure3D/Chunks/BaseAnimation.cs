572a7fd7d95080a6664b30ca039d47ddc4062584
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2025-08-08 11:34:04 +0100

    Add Base Animation chunks

---
+++ src/Pure3D/Chunks/BaseAnimation.cs
@@ -0,0 +1,71 @@
+using System.IO;
+using Godot;
+
+namespace Pure3D.Chunks
+{
+    public class BaseAnimation : Chunk
+    {
+        public uint Version; // I think it's Version at least
+
+        public BaseAnimation(File file, uint type) : base(file, type)
+        {
+        }
+
+        public override void ReadHeader(Stream stream, long length)
+        {
+            if (length != 4)
+            {
+                GD.PrintErr("Base Animation chunk's data is not 4 bytes long!");
+            }
+            else
+            {
+                BinaryReader reader = new(stream);
+                Version = reader.ReadUInt32();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Animation (Version: {Version})";
+        }
+    }
+
+    [ChunkType(88073)]
+    public class EmitterAnimation : BaseAnimation
+    {
+        public EmitterAnimation(File file, uint type) : base(file, type)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Emitter Animation (Version: {Version})";
+        }
+    }
+
+    [ChunkType(88072)]
+    public class ParticleAnimation : BaseAnimation
+    {
+        public ParticleAnimation(File file, uint type) : base(file, type)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Particle Animation (Version: {Version})";
+        }
+    }
+
+    [ChunkType(88074)]
+    public class GeneratorAnimation : BaseAnimation
+    {
+        public GeneratorAnimation(File file, uint type) : base(file, type)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Generator Animation (Version: {Version})";
+        }
+    }
+}
