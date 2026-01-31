a452f1970d1cf2924bede24c309c6b10482b9387
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-01-31 17:31:20 +0000

    Update Bounding Box chunk
    Add ToShortString method

--- src/Pure3D/Chunks/BoundingBox.cs
+++ src/Pure3D/Chunks/BoundingBox.cs
@@ -1,26 +1,24 @@
-﻿using System.IO;
-using System.Text;
-
-namespace Pure3D.Chunks
+﻿namespace Pure3D.Chunks
 {
     [ChunkType(65539)]
-    public class BoundingBox : Chunk
+    public class BoundingBox(File file, uint type) : Chunk(file, type)
     {
         public Vector3 Low;
         public Vector3 High;
 
-        public BoundingBox(File file, uint type) : base(file, type)
-        {
-        }
-
         public override void ReadHeader(Stream stream, long length)
         {
-            BinaryReader reader = new BinaryReader(stream);
+            BinaryReader reader = new(stream);
             Low = Util.ReadVector3(reader);
             High = Util.ReadVector3(reader);
         }
 
         public override string ToString()
+        {
+            return $"Bounding Box (Low: {Low}, High: {High})";
+        }
+
+        public override string ToShortString()
         {
             return "Bounding Box";
         }
