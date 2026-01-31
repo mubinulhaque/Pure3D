7d0f7011ee09941c85576d9654475a07dd60d1b9
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-01-31 16:55:36 +0000

    Update Billboard Perspective Info chunk
    Add ToShortString method

--- src/Pure3D/Chunks/BillboardPerspectiveInfo.cs
+++ src/Pure3D/Chunks/BillboardPerspectiveInfo.cs
@@ -1,27 +1,26 @@
-﻿using System.IO;
-
-namespace Pure3D.Chunks
+﻿namespace Pure3D.Chunks
 {
     [ChunkType(94212)]
-    public class BillboardPerspectiveInfo : Chunk
+    public class BillboardPerspectiveInfo(File file, uint type) : Chunk(file, type)
     {
         public uint Version;
         public uint Perspective;
 
-        public BillboardPerspectiveInfo(File file, uint type) : base(file, type)
-        {
-        }
-
         public override void ReadHeader(Stream stream, long length)
         {
-            BinaryReader reader = new BinaryReader(stream);
+            BinaryReader reader = new(stream);
             Version = reader.ReadUInt32();
             Perspective = reader.ReadUInt32();
         }
 
         public override string ToString()
         {
-            return $"Billboard Perspective Info ({Perspective})";
+            return $"Billboard Perspective: {Perspective})";
+        }
+
+        public override string ToShortString()
+        {
+            return "Billboard Perspective Info";
         }
     }
 }
