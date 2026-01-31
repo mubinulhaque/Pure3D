48fa0a7402e7abaa50a39bb18ae76b8af0ec73d3
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-01-31 16:38:37 +0000

    Update Base Emitter chunk

--- src/Pure3D/Chunks/BaseEmitter.cs
+++ src/Pure3D/Chunks/BaseEmitter.cs
@@ -3,24 +3,41 @@ using System.IO;
 namespace Pure3D.Chunks
 {
     [ChunkType(88069)]
-    public class BaseEmitter : Chunk
+    public class BaseEmitter(File file, uint type) : VersionNamed(file, type)
     {
-        public byte[] Data;
-        private uint unknownType;
-
-        public BaseEmitter(File file, uint type) : base(file, type)
-        {
-            unknownType = type;
-        }
+        public string ParticleType;
+        public string GeneratorType;
+        public bool ZTest;
+        public bool ZWrite;
+        public bool Fog;
+        public uint MaxParticleCount;
+        public bool InfiniteLife;
+        public float RotationalCohesion;
+        public float TranslationalCohesion;
 
         public override void ReadHeader(Stream stream, long length)
         {
-            Data = new BinaryReader(stream).ReadBytes((int)length);
+            base.ReadHeader(stream, length);
+            BinaryReader reader = new(stream);
+            ParticleType = Util.ReadString(reader, 4);
+            GeneratorType = Util.ReadString(reader, 4);
+            ZTest = reader.ReadUInt32() == 1;
+            ZWrite = reader.ReadUInt32() == 1;
+            Fog = reader.ReadUInt32() == 1;
+            MaxParticleCount = reader.ReadUInt32();
+            InfiniteLife = reader.ReadUInt32() == 1;
+            RotationalCohesion = reader.ReadSingle();
+            TranslationalCohesion = reader.ReadSingle();
         }
 
         public override string ToString()
         {
-            return $"Unknown Chunk (TypeID: {unknownType}) (Len: {Data.Length})";
+            return $"{ToShortString()}: {Name} (Particles: {ParticleType}, Generator: {GeneratorType}, Version: {Version})";
+        }
+
+        public override string ToShortString()
+        {
+            return "Base Emitter";
         }
     }
 }
