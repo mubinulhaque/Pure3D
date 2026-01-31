ed841501622ae0b8b9fa396cba21198e9be1302c
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-01-31 17:32:54 +0000

    Update Bounding Sphere chunk
    Add ToShortString method

--- src/Pure3D/Chunks/BoundingSphere.cs
+++ src/Pure3D/Chunks/BoundingSphere.cs
@@ -1,26 +1,24 @@
-﻿using System.IO;
-using System.Text;
-
-namespace Pure3D.Chunks
+﻿namespace Pure3D.Chunks
 {
     [ChunkType(65540)]
-    public class BoundingSphere : Chunk
+    public class BoundingSphere(File file, uint type) : Chunk(file, type)
     {
         public Vector3 Centre;
         public float Radius;
 
-        public BoundingSphere(File file, uint type) : base(file, type)
-        {
-        }
-
         public override void ReadHeader(Stream stream, long length)
         {
-            BinaryReader reader = new BinaryReader(stream);
+            BinaryReader reader = new(stream);
             Centre = Util.ReadVector3(reader);
             Radius = reader.ReadSingle();
         }
 
         public override string ToString()
+        {
+            return $"Bounding Sphere (Centre: {Centre}, Radius: {Radius})";
+        }
+
+        public override string ToShortString()
         {
             return "Bounding Sphere";
         }
