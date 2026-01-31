ceb9fdc77790bd90b857149c502044a22731894f
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-01-31 17:43:03 +0000

    Update Composite Drawable Effect List chunk
    Add ToShortString method

--- src/Pure3D/Chunks/CompositeDrawableEffectList.cs
+++ src/Pure3D/Chunks/CompositeDrawableEffectList.cs
@@ -1,15 +1,12 @@
-﻿using System.IO;
-
-namespace Pure3D.Chunks
+﻿namespace Pure3D.Chunks
 {
+    /// <summary>
+    /// Parent of <c>CompositeDrawableEffect</c> chunks
+    /// </summary>
     [ChunkType(17687)]
-    public class CompositeDrawableEffectList : Chunk
+    public class CompositeDrawableEffectList(File file, uint type) : Chunk(file, type)
     {
         public uint NumElements; // should be # of children.
-
-        public CompositeDrawableEffectList(File file, uint type) : base(file, type)
-        {
-        }
 
         public override void ReadHeader(Stream stream, long length)
         {
@@ -19,6 +16,11 @@ namespace Pure3D.Chunks
         public override string ToString()
         {
             return $"Composite Drawable Effect List ({NumElements} Elements)";
+        }
+
+        public override string ToShortString()
+        {
+            return $"{NumElements} Composite Drawable Effects";
         }
     }
 }
