66ce79aac10d9c8605f0487766d600c270cd6827
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-01-31 17:39:15 +0000

    Update Colour List chunk
    Add ToShortString method

--- src/Pure3D/Chunks/ColourList.cs
+++ src/Pure3D/Chunks/ColourList.cs
@@ -1,20 +1,13 @@
-﻿using System.IO;
-using System.Text;
-
-namespace Pure3D.Chunks
+﻿namespace Pure3D.Chunks
 {
     [ChunkType(65544)]
-    public class ColourList : Chunk
+    public class ColourList(File file, uint type) : Chunk(file, type)
     {
         public uint[] Colours;
 
-        public ColourList(File file, uint type) : base(file, type)
-        {
-        }
-
         public override void ReadHeader(Stream stream, long length)
         {
-            BinaryReader reader = new BinaryReader(stream);
+            BinaryReader reader = new(stream);
             uint len = reader.ReadUInt32();
             Colours = new uint[len];
             for (int i = 0; i < len; i++)
@@ -23,7 +16,12 @@ namespace Pure3D.Chunks
 
         public override string ToString()
         {
-            return $"Colour List ({Colours.Length})";
+            return $"Colour List ({Colours.Length} Colours)";
+        }
+
+        public override string ToShortString()
+        {
+            return $"{Colours.Length} Colours";
         }
     }
 }
