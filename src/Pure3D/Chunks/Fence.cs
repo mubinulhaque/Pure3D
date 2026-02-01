a7bbe06c139feb9548bf0a1ba92d09f136095a11
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-02-01 18:38:15 +0000

    Add Fence chunks
    Add Fence and Fence 2 chunks

---
+++ src/Pure3D/Chunks/Fence.cs
@@ -0,0 +1,49 @@
+namespace Pure3D.Chunks
+{
+    /// <summary>
+    /// Parent of <c>Fence 2</c> chunks
+    /// </summary>
+    [ChunkType(66060295)]
+    public class Fence(File file, uint type) : Chunk(file, type)
+    {
+        public override void ReadHeader(Stream stream, long length)
+        {
+        }
+
+        public override string ToString()
+        {
+            return ToShortString();
+        }
+
+        public override string ToShortString()
+        {
+            return "Fence";
+        }
+    }
+
+    [ChunkType(50331648)]
+    public class Fence2(File file, uint type) : Chunk(file, type)
+    {
+        public Vector3 Start;
+        public Vector3 End;
+        public Vector3 Normal;
+
+        public override void ReadHeader(Stream stream, long length)
+        {
+            BinaryReader reader = new(stream);
+            Start = Util.ReadVector3(reader);
+            End = Util.ReadVector3(reader);
+            Normal = Util.ReadVector3(reader);
+        }
+
+        public override string ToString()
+        {
+            return $"{ToShortString()}: ({Start} - {End})";
+        }
+
+        public override string ToShortString()
+        {
+            return "Fence 2";
+        }
+    }
+}
