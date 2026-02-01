c115dc43e2bf02350cd806a7da139ba0d7dc0254
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-02-01 18:55:35 +0000

    Add Instanced Particle System chunk

---
+++ src/Pure3D/Chunks/InstancedParticleSystem.cs
@@ -0,0 +1,26 @@
+namespace Pure3D.Chunks
+{
+    [ChunkType(50335745)]
+    public class InstancedParticleSystem(File file, uint type) : Chunk(file, type)
+    {
+        public uint Index;
+        public uint NumberOfInstances;
+
+        public override void ReadHeader(Stream stream, long length)
+        {
+            BinaryReader reader = new(stream);
+            Index = reader.ReadUInt32();
+            NumberOfInstances = reader.ReadUInt32();
+        }
+
+        public override string ToString()
+        {
+            return $"{ToShortString()}: {Index} ({NumberOfInstances} Instances)";
+        }
+
+        public override string ToShortString()
+        {
+            return "Instanced Particle System";
+        }
+    }
+}
