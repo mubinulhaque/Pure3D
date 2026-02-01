b55d876e3e27e9aed30f50444a2b6fa07bfccf3f
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-02-01 18:36:17 +0000

    Add Entity chunks
    Add 7 Entity chunks

---
+++ src/Pure3D/Chunks/Entity.cs
@@ -0,0 +1,88 @@
+namespace Pure3D.Chunks
+{
+    public abstract class Entity(File file, uint type) : VersionNamed(file, type) // I think it's VersionNamed, at least
+    {
+        public uint RenderOrder;
+
+        public override void ReadHeader(Stream stream, long length)
+        {
+            BinaryReader reader = new(stream);
+            Name = Util.ReadString(reader);
+            Version = reader.ReadUInt32();
+            RenderOrder = reader.ReadUInt32();
+        }
+
+        public override string ToString()
+        {
+            return $"{ToShortString()}: {Name} (Render Order: {RenderOrder}, Version {Version})";
+        }
+
+        public override string ToShortString()
+        {
+            return "Entity";
+        }
+    }
+
+    [ChunkType(66060296)]
+    public class AnimatedCollision(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Animated Collision";
+        }
+    }
+
+    [ChunkType(66060302)]
+    public class AnimatedDynamicPhysicsObject(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Animated Dynamic Physics Object";
+        }
+    }
+
+    [ChunkType(66060300)]
+    public class AnimatedEntity(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Animated Entity";
+        }
+    }
+
+    [ChunkType(66060290)]
+    public class DynamicPhysicsObject(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Dynamic Physics Object";
+        }
+    }
+
+    [ChunkType(66060297)]
+    public class InstaStaticEntity(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Instanced Static Entity";
+        }
+    }
+
+    [ChunkType(66060298)]
+    public class InstaStaticPhysicsObject(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Instanced Static Physics Object";
+        }
+    }
+
+    [ChunkType(66060288)]
+    public class StaticEntity(File file, uint type) : Entity(file, type)
+    {
+        public override string ToShortString()
+        {
+            return "Static Entity";
+        }
+    }
+}
