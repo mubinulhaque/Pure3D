8da30b1f82271ebe488b108f1d006b16609c6632
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2026-02-01 19:12:12 +0000

    Finish Locator chunks
    Add missing properties and Locator 2 chunk

--- src/Pure3D/Chunks/Locator.cs
+++ src/Pure3D/Chunks/Locator.cs
@@ -1,14 +1,60 @@
-﻿using System.IO;
-
-namespace Pure3D.Chunks
+﻿namespace Pure3D.Chunks
 {
+    /// <summary>
+    /// Currently unused
+    /// </summary>
     [ChunkType(81920)]
-    public class Locator : Named
+    public class Locator(File file, uint type) : VersionNamed(file, type)
     {
-        public uint Version;
         public Vector3 Position;
 
-        public Locator(File file, uint type) : base(file, type)
+        public override void ReadHeader(Stream stream, long length)
+        {
+            BinaryReader reader = new BinaryReader(stream);
+            Name = Util.ReadString(reader);
+            Version = reader.ReadUInt32();
+            Position = Util.ReadVector3(reader);
+        }
+
+        public override string ToString()
+        {
+            return $"Locator: {Name} (Position: ({Position}), Version: {Version})";
+        }
+
+        public override string ToShortString()
+        {
+            return "Locator";
+        }
+    }
+
+    /// <summary>
+    /// Used for spawning objects in the world at a specific position
+    /// </summary>
+    [ChunkType(50331653)]
+    public class Locator2 : Named
+    {
+        /// <summary>
+        /// <para>Number from 0 to 15 that determines the object of the Locator</para>
+        /// </summary>
+        public Types LocatorType;
+        /// <summary>
+        /// Size of the <c>Data</c> array
+        /// </summary>
+        public uint DataSize;
+        /// <summary>
+        /// Collection of elements four bytes each
+        /// </summary>
+        public Locator2Data[] Data;
+        /// <summary>
+        /// Position of the Locator
+        /// </summary>
+        public Vector3 Position;
+        /// <summary>
+        /// Amount of Trigger Volumes attached
+        /// </summary>
+        public uint NumTriggers;
+
+        public Locator2(File file, uint type) : base(file, type)
         {
         }
 
@@ -16,13 +62,127 @@ namespace Pure3D.Chunks
         {
             BinaryReader reader = new BinaryReader(stream);
             base.ReadHeader(stream, length);
-            Version = reader.ReadUInt32();
+            LocatorType = (Types)reader.ReadUInt32();
+            DataSize = reader.ReadUInt32();
+            Data = new Locator2Data[DataSize];
+
+            for (int i = 0; i < DataSize; i++)
+            {
+                Data[i] = new Locator2Data(
+                    reader.ReadByte(),
+                    reader.ReadByte(),
+                    reader.ReadByte(),
+                    reader.ReadByte()
+                );
+            }
+
             Position = Util.ReadVector3(reader);
+            NumTriggers = reader.ReadUInt32();
         }
 
         public override string ToString()
         {
-            return $"Locator: {Name}";
+            return $"{Name} Locator Type: {LocatorType} ({NumTriggers} Triggers)";
+        }
+
+        public override string ToShortString()
+        {
+            return "Locator 2";
+        }
+
+        public enum Types : uint
+        {
+            /// <summary>
+            /// Triggers events
+            /// </summary>
+            EVENT,
+            /// <summary>
+            /// Triggers scripts
+            /// </summary>
+            SCRIPT,
+            /// <summary>
+            /// Wasps, gag models and gag triggers
+            /// </summary>
+            WASP_GAG,
+            /// <summary>
+            /// Cars and characters
+            /// </summary>
+            CAR_START,
+            SPLINE,
+            /// <summary>
+            /// Loads/unloads specific parts of the world
+            /// </summary>
+            WORLD_LOADING,
+            OCCLUSION,
+            INTERIOR,
+            /// <summary>
+            /// Defines player's position upon entering an interior
+            /// </summary>
+            PLAYER_POS,
+            /// <summary>
+            /// Creates an Action that the player can trigger
+            /// </summary>
+            ACTION,
+            FOV,
+            /// <summary>
+            /// Unused in Hit & Run
+            /// </summary>
+            UNUSED,
+            CAMERA,
+            /// <summary>
+            /// Changes active <c>Ped Group</c>
+            /// </summary>
+            PED,
+            COIN,
+            /// <summary>
+            /// Spawns a Wasp Camera at a <c>WASP_GAG</c> Locator
+            /// </summary>
+            WASP_SPAWN
+        }
+    }
+
+    /// <summary>
+    /// Four bytes that can be translated into different data types
+    /// </summary>
+    public class Locator2Data(byte byte1, byte byte2, byte byte3, byte byte4)
+    {
+        /// <summary>
+        /// Four bytes to be converted into a different data type
+        /// </summary>
+        public byte[] Bytes =
+            [
+                byte1,
+                byte2,
+                byte3,
+                byte4
+            ];
+
+        public override string ToString()
+        {
+            return $"{Bytes[0]}, {Bytes[1]}, {Bytes[2]}, {Bytes[3]}";
+        }
+    }
+
+    [ChunkType(50331660)]
+    public class LocatorMatrix(File file, uint type) : Chunk(file, type)
+    {
+        public Matrix Transform;
+
+        public override void ReadHeader(Stream stream, long length)
+        {
+            BinaryReader reader = new(stream);
+            Transform = Util.ReadMatrix(reader);
+        }
+
+        public override string ToString()
+        {
+            string[] matrix = Util.PrintMatrix(Transform);
+            return $"{ToShortString()} (Transform: {matrix[0]}, {matrix[1]}, {matrix[2]}, {matrix[3]})";
+        }
+
+        public override string ToShortString()
+        {
+            return "Locator Matrix";
         }
     }
 }
